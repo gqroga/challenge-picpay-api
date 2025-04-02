aea4047bfafa1f7a6847d433462eb89df94e8a6c
gqroga <gcqueirogat@gmail.com>
2025-04-01 23:51:04 -0300

    first commit

---
+++ src/test/java/com/challenge_picpay_api/ChallengePicpayApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.challenge_picpay_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengePicpayApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
